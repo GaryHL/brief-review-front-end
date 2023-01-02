273454ce70006d6ca07a662d00b6cfdebbd42efe
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-02 10:13:14 +0100

    style: :lipstick: change styles in formlogin
    refactorizate styles and add responsive

--- src/components/formLogin/FormLogin.tsx
+++ src/components/formLogin/FormLogin.tsx
@@ -1,16 +1,19 @@
-import './formLogin.scss';
+import { AiOutlineUser } from "react-icons/ai";
+import { RiLockPasswordLine } from "react-icons/ri";
+import "./formLogin.scss";
 
 const FormLogin = () => {
    return (
       <div className="container_form">
          <div className="line"></div>
          <form action="" className="form_login">
-            <label htmlFor="password" className="container_user">
-               user
+            <label htmlFor="password" className="container_input">
+               <AiOutlineUser />
                <input name="user" type="text" placeholder="User" />
             </label>
-            <label htmlFor="password" className="container_password">
-               password<input name="password" type="password" placeholder="Password" />
+            <label htmlFor="password" className="container_input">
+               <RiLockPasswordLine />
+               <input name="password" type="password" placeholder="Password" />
             </label>
             <button className="form_submit" type="submit">
                LOGIN
