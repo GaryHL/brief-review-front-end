aabb047636a3e9da4aba928387ad01ef22b141aa
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-07 19:13:21 +0100

    feat: :construction: create protected routes
    add news protected routes, if the person does not have their token in the local storage they cannot enter

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Register from '../views/auth/register/Register'
+import Register from "../views/auth/register/Register";
 import Login from "../views/auth/login/Login";
 import Home from "../views/home/Home";
 
@@ -9,10 +9,48 @@ const Router = () => {
    return (
       <BrowserRouter>
          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/Register" element={<Register />} />
-            <Route path="/home" element={<Home/>} />
+            <Route
+               path="/home"
+               element={
+                  localStorage.getItem("auth_token") ? (
+                     <Navigate to="/login" />
+                     ) : (
+                     <Home />
+                  )
+               }
+            />
+
+            <Route
+               path="/"
+               element={
+                  localStorage.getItem("auth_token") ? (
+                     <Home />
+                  ) : (
+                     <Navigate to="/login" />
+                  )
+               }
+            />
+            <Route
+               path="/login"
+               element={
+                  localStorage.getItem("auth_token") ? (
+                     <Navigate to="/" />
+                  ) : (
+                     <Login />
+                  )
+               }
+            />
+            <Route
+               path="/Register"
+               element={
+                  localStorage.getItem("auth_token") ? (
+                     <Navigate to="/" />
+                  ) : (
+                     <Register />
+                  )
+               }
+            />
+
             <Route path="/chat" />
             <Route path="/select-tech" />
             <Route path="/briefs" />
