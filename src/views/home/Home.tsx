b1adde1c0479f3fbe3121282e67f5d4bce51e8b0
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-08 23:24:33 +0100

    feat: :art: change home structure
    modify, innecesary tags and, change the css

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React ,{useEffect}from "react";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../../components/navBar/NavBar";
 import "./home.scss";
+import FormPost from "../../components/formPost/FormPost";
+import ContainerFeed from "../../components/containerFeed/ContainerFeed";
 
 const Home = () => {
+   useEffect(() => {
+      const navigate = useNavigate();
+      localStorage.getItem("auth_token") ? null : navigate("/login");
+   }, []);
+
    return (
       <>
          <NavBar />
          <div className="container_home">
-            <h1>Home page</h1>
+            <div className=""></div>
+            <div className="">
+               <FormPost />
+               <ContainerFeed />
+            </div>
+            <div className=""></div>
          </div>
       </>
    );
