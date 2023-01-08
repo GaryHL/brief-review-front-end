bc0277d1ea8eff60908ea7c2f6cd917202516668
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-08 23:12:29 +0100

    refactor: :recycle: adding log out functionality
    remove access token from api and localstorage

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,8 +1,9 @@
 import "./navBar.scss";
 import { CgMenuGridR } from "react-icons/cg";
-import {useNavigate} from 'react-router-dom';
+import { AiOutlineDoubleLeft } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 import { useBoolean } from "../../hooks/useBoolean";
-import { Link } from "react-router-dom";
+import { Link,Navigate } from "react-router-dom";
 import axios from "axios";
 import swal from "sweetalert";
 
@@ -11,50 +12,53 @@ const NavBar = () => {
 
    const navigate = useNavigate();
 
-   const logoutSubmit = (e:React.FormEvent) =>{
-
+   const logoutSubmit = (e: React.FormEvent) => {
       e.preventDefault();
 
-      axios.post('http://127.0.0.1:8000/api/logout').then(res=>{
-         if(res.data.status === 200){
-            localStorage.removeItem('auth_token');
-            localStorage.removeItem('auth_name');
-            swal("Success",res.data.message,"success");
-            navigate('/login');
+      axios.post("http://127.0.0.1:8000/api/logout").then((res) => {
+         if (res.data.status === 200) {
+            localStorage.removeItem("auth_token");
+            localStorage.removeItem("auth_name");
+            swal("Success", res.data.message, "success");
+            navigate("/login");
             // window.location.reload();
          }
       });
-
-
-   }
+   };
 
    return (
       <>
          <div className={openNavbar.boolean ? "menu open" : "menu close"}>
             <div className="menu_header">
                <h2 className="logo">Brief Review</h2>
-               <CgMenuGridR
+               <AiOutlineDoubleLeft
                   className="btn_menu"
                   onClick={() => openNavbar.setTogle()}
                />
             </div>
             <ul className="options_menu">
-               <li>
-                  <Link to="/login" className="option_menu">
-                     Iniciar sesion
+               <li className="option_menu">
+                  <Link to="/" className="option_menu">
+                     Home
                   </Link>
                </li>
-               <li>
-                  <Link to="/register" className="option_menu">
-                     Registrarse
+               <li className="option_menu">
+                  <Link to="/profile" className="option_menu">
+                     Mi Perfil
                   </Link>
                </li>
-               <li className="option_menu logout" onClick={logoutSubmit}>Cerrar sesion</li>
+               
+               <li className="option_menu" onClick={logoutSubmit}>
+                  Cerrar sesion
+               </li>
             </ul>
          </div>
          <nav className="container_navbar">
             <h2 className="logo">Brief Review</h2>
-            <CgMenuGridR onClick={() => openNavbar.setTogle()} />
+            <CgMenuGridR
+               className="btn_menu"
+               onClick={() => openNavbar.setTogle()}
+            />
          </nav>
       </>
    );
