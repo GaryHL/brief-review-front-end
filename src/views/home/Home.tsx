2d28c3462e0ce970f6651051d0292b4e9fe99540
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-09 11:18:30 +0100

    feat: :lipstick: add styles
    effect hover and focus

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -6,21 +6,21 @@ import FormPost from "../../components/formPost/FormPost";
 import ContainerFeed from "../../components/containerFeed/ContainerFeed";
 
 const Home = () => {
-   useEffect(() => {
-      const navigate = useNavigate();
-      localStorage.getItem("auth_token") ? null : navigate("/login");
-   }, []);
+   // useEffect(() => {
+   //    const navigate = useNavigate();
+   //    localStorage.getItem("auth_token") ? null : navigate("/login");
+   // }, []);
 
    return (
       <>
          <NavBar />
          <div className="container_home">
-            <div className=""></div>
+            <div className="">div vacio</div>
             <div className="">
                <FormPost />
                <ContainerFeed />
             </div>
-            <div className=""></div>
+            <div className="">div vacio</div>
          </div>
       </>
    );
